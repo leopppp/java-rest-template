c98d6cf694fe62cfeeca56285781643494deb7b7
leopppp <leopeng.nz@gmail.com>
2019-05-11 19:09:37 +1000

    update

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 # Build API from source code (Prerequisite: Have JDK 1.8 and maven installed)
 
+#Change current directory to the root directoy of java-rest-template repository
 mvn clean install # Creates jar in target->resttemplate-1.0.jar
 # To run the jar on local, you can run this command -> java -jar target/resttemplate-1.0.jar
 
@@ -14,14 +15,19 @@ docker push leopppp/java-rest-template:1.0
 # Deploy image to Kubernetes
 #(Prerequisites:
 # 1. docker image should be present in a registry from where Kubernetes should be able to download it)
-# 2. kubectl should be set up on local
-kubectl create -f deploy.yml --save-config
-kubectl expose deployment rest-template-deployment --type=LoadBalancer --name=rest-template-service  --port=80 --target-port=8080
+# 2. Google cloud SDK and kubectl is setup locally
+#kubectl create -f deploy.yml --save-config
+#kubectl expose deployment rest-template-deployment --type=LoadBalancer --name=rest-template-service  --port=80 --target-port=8080
+
+#Update Kubernetes container image
+kubectl set image deployments/rest-template-deployment resttemplate=leopppp/java-rest-template:1.0
 
 # Then you can access the REST API like the following:
 # http://<load balancer ip>
 # http://<load balancer ip>/status
 
 # Port forwarding to your local computer
+#gcloud container clusters get-credentials standard-cluster-1 --zone australia-southeast1-a --project leoproject-239411 \
+ #&& kubectl port-forward $(kubectl get pod --selector="app=resttemplate" --output jsonpath='{.items[0].metadata.name}') 8080:8080
 
 
