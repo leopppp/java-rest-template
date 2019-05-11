b404fd64018732fdade0089c49f5ecd3225d9c4c
leopppp <leopeng.nz@gmail.com>
2019-05-11 15:52:25 +1000

    update stages

--- src/main/java/resttemplate/ConfigUtil.java
+++ src/main/java/resttemplate/ConfigUtil.java
@@ -1,8 +1,5 @@
 package resttemplate;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
